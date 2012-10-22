365c012771b1135baa3a582e62f86b18e24735c1
Ivo van der Wijk <github@in.m3r.nl>
2012-10-22 16:57:18 +0200

    start writing tests

---
+++ two/ol/tests/test_dispatcher.py
@@ -0,0 +1,33 @@
+from two.ol.base import BaseDispatcher, RESTLikeHandler, FormHandler
+from two.ol.base import FormDispatcher, RESTLikeDispatcher
+
+from django.test.client import RequestFactory
+
+class TestRESTHandler(RESTLikeHandler):
+    pass
+
+class TestFormHandler(FormHandler):
+    pass
+
+class TestFormDispatcher(FormDispatcher):
+    pass
+
+class TestRESTLikeDispatcher(RESTLikeDispatcher):
+    pass
+
+class TestDispatcher(object):
+    def setup(self):
+        self.factory = RequestFactory()
+
+    def test_one(self, client):
+        request = self.factory.get('/foo')
+        instance = object()
+        post = False
+        rest = ""
+        path = ""
+
+        h = TestRESTHandler # (request, instance, post, rest, path)
+        d = TestRESTLikeDispatcher(h, 'foo')
+
+        d(request, "/foo/bar", a="bla")
+
