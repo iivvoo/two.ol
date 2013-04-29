89ba51bbcfa5cbcb8ab1dbd4c2a1a42dd47dd0b0
Ivo van der Wijk <github@in.m3r.nl>
2013-04-29 14:31:07 +0200

    utility method to get remote ip

--- two/ol/util.py
+++ two/ol/util.py
@@ -15,3 +15,11 @@ def classproperty(f):
         def __get__(self, obj, type=None):
             return self.getter(type)
     return name(f)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
